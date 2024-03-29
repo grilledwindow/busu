97a4c1970cb032cacc4b8918b9642a86d5a72c50
Xavier Ang <xaveang@gmail.com>
2024-03-29 20:03:28 +0800

    Add BusArrival type

--- src/routes/api/bus/+server.ts
+++ src/routes/api/bus/+server.ts
@@ -1,6 +1,36 @@
 import { DATAMALL_KEY } from '$env/static/private'
 
-export const GET = (e) => {
+interface NextBus {
+  OriginCode: string;
+  DestinationCode: string;
+  EstimatedArrival: string;
+  Latitude: string;
+  Longitude: string;
+  VisitNumber: string;
+  Load: string;
+  Feature: string;
+  Type: string;
+}
+
+interface BusService {
+  ServiceNo: string;
+  Operator: string;
+  NextBus: NextBus;
+  NextBus2: NextBus;
+  NextBus3: NextBus;
+}
+
+export interface BusArrival {
+  "odata.metadata": string;
+  BusStopCode: string;
+  Services: BusService[];
+}
+
+export interface BusArrivalResponse extends Response {
+  json: () => Promise<BusArrival>;
+}
+
+export const GET = (e): Promise<BusArrivalResponse> => {
   return fetch('http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139', {
     headers: { 'accountKey': DATAMALL_KEY }
   });
